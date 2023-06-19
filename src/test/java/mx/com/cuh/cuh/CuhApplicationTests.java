c7bb90bcb4ca444fe850dd78c0f8b07ce3b5b860
Carlos Ruhal Sebastian Ruiz Perez <sebastianperezruiz6@gmail.com>
2023-06-19 14:21:55 -0600

    Commit inicial

---
+++ src/test/java/mx/com/cuh/cuh/CuhApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.cuh.cuh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
