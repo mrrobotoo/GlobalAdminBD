fa0f630308364481394b756ce5061b824441d156
DESKTOP-AR0LULB\DILLAN <cuh51208098@cuh.mx>
2023-06-21 14:23:40 -0600

    se agrega persona espero poder mandar el delete mas tarde equipo 2

--- src/main/java/mx/com/cuh/globlal/dto/PersonasDTO.java
+++ src/main/java/mx/com/cuh/globlal/dto/PersonasDTO.java
@@ -1,5 +1,36 @@
 package mx.com.cuh.globlal.dto;
 
 public class PersonasDTO {
+	
+	private String Nombre;
+	private String Edad;
+	private Long IdPerson;
+	private String sexo;
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getEdad() {
+		return Edad;
+	}
+	public void setEdad(String edad) {
+		Edad = edad;
+	}
+	public Long getIdPerson() {
+		return IdPerson;
+	}
+	public void setIdPerson(Long idPerson) {
+		IdPerson = idPerson;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	
 
 }
