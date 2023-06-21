7b6ee6c54232eba80a5908457d76c2160b925257
Carlos Portillo <cuh51207989@cuh.mx>
2023-06-21 10:54:05 -0600

    borrar personas

--- src/main/java/mx/com/cuh/global/controller/RestController.java
+++ src/main/java/mx/com/cuh/global/controller/RestController.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import mx.com.cuh.global.dto.PersonaDTO;
 import mx.com.cuh.global.entity.TbPerson;
@@ -20,5 +22,11 @@ public class RestController {
 	@GetMapping(value = "/person")
 	public Respuesta<TbPerson> listaPersonas() {
 		return usuario.obtenerPersonas();
+	}
+	
+	@DeleteMapping(value = "/borrarPersona")
+
+	public Respuesta<String> borrarPersonas(@RequestParam Long id) {
+		return usuario.borrarPersona(id);
 	}
 }
