cea408af3ee496a66c18eea0e6dfc51863d7cfad
alexvela <cuh51207903@cuh.mx>
2023-06-21 19:15:45 -0600

    no message

--- src/main/java/mx/com/cuh/global/dto/PersonaDTO.java
+++ src/main/java/mx/com/cuh/global/dto/PersonaDTO.java
@@ -1,23 +1,41 @@
 package mx.com.cuh.global.dto;
 
-public class PersonaDTO {
-	        private String Nombre;
-	        private String Edad;
-	        private String Sexo;
-	        
-	     public String getNombre() {
-				return Nombre;
-			}
-		public String getEdad() {
-			return Edad;
+	import javax.persistence.*;
+
+	@Entity
+	@Table(name = "personas")
+	public class PersonaDTO { 
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+
+	    private String nombre;
+	    private int edad;
+	    private String sexo;
+	 // Constructores, getters y setters
+		public Long getId() {
+			return id;
 		}
-		public void setEdad(String edad) {
-			Edad = edad;
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getNombre() {
+			return nombre;
+		}
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+		public int getEdad() {
+			return edad;
+		}
+		public void setEdad(int edad) {
+			this.edad = edad;
 		}
 		public String getSexo() {
-			return Sexo;
+			return sexo;
 		}
 		public void setSexo(String sexo) {
-			Sexo = sexo;
+			this.sexo = sexo;
 		}
-}
+	    
+	}
