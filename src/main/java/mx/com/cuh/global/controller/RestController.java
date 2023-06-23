50795d31868507ab52660cf676a513a092f659b7
Vane Guevara <cuh51208002@cuh.mx>
2023-06-22 18:50:26 -0600

    Método actualizar 22-Junio-2023

--- src/main/java/mx/com/cuh/global/controller/RestController.java
+++ src/main/java/mx/com/cuh/global/controller/RestController.java
@@ -1,18 +1,20 @@
 package mx.com.cuh.global.controller;
 
-import java.util.ArrayList;
-import java.util.List;
+
+
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import mx.com.cuh.global.dto.PersonaDTO;
 import mx.com.cuh.global.entity.TbPerson;
+import mx.com.cuh.global.repository.TbPersonRepository;
 import mx.com.cuh.global.service.Usuario;
 import mx.com.cuh.global.dto.Respuesta;
 
@@ -42,9 +44,15 @@ public class RestController {
 
 	@PutMapping(value = "/actperson")
 	public Respuesta<String> actualizarPersonas(@RequestParam 
-			Long id ) {
+			Long id, PersonaDTO persona ) {
 			return usuario.actualizarPersona(id); 
 
 	}
+
 }
+	
+
+
+
+
  
