12b55d77b225b45f43aca8bfac7f213777da7c68
DESKTOP-AR0LULB\DILLAN <cuh51208098@cuh.mx>
2023-07-04 08:27:04 -0600

    para angel 2

--- java/mx/com/cuh/global/controller/Controller.java
+++ java/mx/com/cuh/global/controller/Controller.java
@@ -1,7 +1,30 @@
 package mx.com.cuh.global.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import mx.com.cuh.global.service.usuario;
+import mx.com.cuh.globlal.dto.PersonasDTO;
+import mx.com.cuh.globlal.dto.Respuesta;
+
 @org.springframework.stereotype.Controller
 
 public class Controller {
+	
+	@Autowired
+    private usuario usuario;
 
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@PostMapping(value = "/saveperson")
+    public String insertarPersona(
+            @ModelAttribute PersonasDTO personas) {
+        usuario.insertarPersona(personas);
+        return "user";
+    }
 }
