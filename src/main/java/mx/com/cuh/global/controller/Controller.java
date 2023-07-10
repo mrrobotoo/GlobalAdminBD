5508738b166aa324a471d483d8ac9f5084406c2b
Jhon E. Alt <wlemma22@gmail.com>
2023-07-10 07:51:39 -0600

    null

--- src/main/java/mx/com/cuh/global/controller/Controller.java
+++ src/main/java/mx/com/cuh/global/controller/Controller.java
@@ -1,13 +1,15 @@
 package mx.com.cuh.global.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import mx.com.cuh.global.dto.PersonasDTO;
-import mx.com.cuh.global.dto.Respuesta;
+import mx.com.cuh.global.entity.TbPersonas;
 import mx.com.cuh.global.service.User;
 
 @org.springframework.stereotype.Controller
@@ -21,10 +23,24 @@ public class Controller {
 	public String index(){
 		return"index";
 	}
+	
+	
+	@GetMapping("/inicio")
+	public String inicio(Model model){
+		user.obtenerRegistros();
+		List<TbPersonas> listaPersonas = 
+				user.obtenerRegistros().getListaPersonas();
+		model.addAttribute("listaPersonas",listaPersonas);
+		return"inicio";
+	}
+	
     @PostMapping(value = "/saveperson")
     public String insertarPersona(
             @ModelAttribute PersonasDTO persona) {
         user.insertarPersona(persona);
         return "user";
     }
+    
+
+    
 }
