a100e83bfa10cac0b8c67d2cfe008a73c67e994b
Jhon E. Alt <wlemma22@gmail.com>
2023-07-13 19:41:35 -0600

    13/07/2023 :3

--- src/main/java/mx/com/cuh/global/controller/Controller.java
+++ src/main/java/mx/com/cuh/global/controller/Controller.java
@@ -1,18 +1,25 @@
 package mx.com.cuh.global.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import mx.com.cuh.global.dto.PersonasDTO;
+import mx.com.cuh.global.dto.Respuesta;
 import mx.com.cuh.global.entity.TbPersonas;
 import mx.com.cuh.global.repository.TbPersonasRepository;
 import mx.com.cuh.global.service.User;
@@ -41,7 +48,7 @@ public class Controller {
 	
     @PostMapping(value = "/saveperson")
     public String insertarPersona(
-            @ModelAttribute PersonasDTO persona) {
+            @ModelAttribute PersonasDTO persona, RedirectAttributes attributes) {
         user.insertarPersona(persona);
         return "user";
     }
@@ -50,9 +57,14 @@ public class Controller {
     public String eliminar(@PathVariable
     		Long idUser){
     	user.borrar(idUser);
-    	
     	return "redirect:/inicio";
     	}
-
     
-}
+    @PostMapping(value = "/actualizar/{idUser}")
+    public String actualizarPersona(@PathVariable Long idUser, 
+        @ModelAttribute PersonasDTO persona) {
+        user.actualizarPersona(idUser, persona);
+        return "redirect:/inicio";
+    }
+       
+    }
