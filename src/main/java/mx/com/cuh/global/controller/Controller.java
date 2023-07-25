cd401f5a42ff5619dd625b677137307f9754c180
DESKTOP-AR0LULB\DILLAN <cuh51208098@cuh.mx>
2023-07-24 18:57:40 -0600

    24/072023

--- src/main/java/mx/com/cuh/global/controller/Controller.java
+++ src/main/java/mx/com/cuh/global/controller/Controller.java
@@ -1,10 +1,28 @@
 package mx.com.cuh.global.controller;
 
+import java.io.ByteArrayOutputStream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,63 +31,64 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+
 import mx.com.cuh.global.dto.PersonasDTO;
-import mx.com.cuh.global.dto.Respuesta;
 import mx.com.cuh.global.entity.TbPersonas;
+import mx.com.cuh.global.repository.TbPersonasRepository;
 import mx.com.cuh.global.service.User;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 	@Autowired
 	private User user;
-	
- @RequestMapping("/")
-	 public String index() {
-		 return "index";
-	 }
- 
 
- @PostMapping(value = "/saveperson")
- public String insertarPersona(
-         @ModelAttribute PersonasDTO persona) {
-     user.insertarPersona(persona);
-     return  "user";
- 	} 
- 
- 
+	@Value("${cosa.cosa")
+	private String valor;
 
-@PostMapping(value = "/actualizar/{idUser}")
-public String actualizarPersona(@PathVariable
-		Long idUser, 
-	@ModelAttribute PersonasDTO persona) {
-	user.actualizarPersona(idUser, persona);
-    return "redirect:/inicio";
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@PostMapping(value = "/saveperson")
+	public String insertarPersona(@ModelAttribute PersonasDTO persona) {
+		user.insertarPersona(persona);
+		return "user";
+	}
+
+	@GetMapping("/eliminar/{idUser}")
+	public String eliminar(@PathVariable Long idUser) {
+		user.borrar(idUser);
+		return "redirect:/inicio";
+	}
+          
+	@PostMapping(value = "/actualizar/{idUser}")
+	public String actualizarPersona(@PathVariable Long idUser, @ModelAttribute PersonasDTO persona) {
+		user.actualizarPersona(idUser, persona);
+		return "redirect:/inicio";
+	}
+
+	@GetMapping("inicio")
+	public String inicio(Model model, @RequestParam(defaultValue = "0") int page) {
+		if (page < 0) {
+			return "redirect:/inicio";
+		}
+		int pageSize = 10;
+		PageRequest pageRequest = PageRequest.of(page, pageSize);
+		Page<TbPersonas> personasPage = user.obtenerRegistroPaginados(pageRequest);
+		int totalPages = personasPage.getTotalPages();
+		if (page >= totalPages) {
+			return "redirect:/inicio?page=" + (totalPages - 1);
+		}
+		model.addAttribute("listaPersonas", personasPage.getContent());
+		model.addAttribute("currentPage", page);
+		model.addAttribute("totalPages", personasPage.getTotalPages());
+		return "inicio";
+	}
+
+	@GetMapping("/exportar-pdf-zip")
+	public ResponseEntity<ByteArrayResource> exportarPdfZip() {
+		return user.exportarPdfZip();
+	}
 }
-@GetMapping("inicio")
-public String inicio(Model model, @RequestParam(defaultValue = "0") int page) {
-    if (page < 0) {
-           return "redirect:/inicio";
-       }
-    int pageSize = 10;
-    PageRequest pageRequest = PageRequest.of(page, pageSize);
-    Page<TbPersonas> personasPage = user.obtenerRegistroPaginados(pageRequest);
-    int totalPages = personasPage.getTotalPages();
-    if (page >= totalPages) {
-        return "redirect:/inicio?page=" + (totalPages - 1);
-    }
-    model.addAttribute("listaPersonas", personasPage.getContent());
-    model.addAttribute("currentPage", page);
-    model.addAttribute("totalPages", personasPage.getTotalPages());
-    return "inicio";
-}
-
-@GetMapping("/eliminar/{idUser}")
-public String eliminar(@PathVariable
-    Long idUser) {
- user.borrar(idUser);
-	return "redirect:/inicio";
-}
-
-
- }
-
