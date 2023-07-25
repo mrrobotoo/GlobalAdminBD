5f9ec0885fce76aacbd9614aaac842e394901ce6
Carlos Portillo <cuh51207989@cuh.mx>
2023-07-25 07:28:49 -0600

    codigo coreccto PDF exportar

--- src/main/java/mx/com/cuh/global/controller/Controller.java
+++ src/main/java/mx/com/cuh/global/controller/Controller.java
@@ -1,7 +1,13 @@
 package mx.com.cuh.global.controller;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -15,12 +21,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
-
 import mx.com.cuh.global.dto.PersonaDTO;
-
+import mx.com.cuh.global.dto.Respuesta;
 import mx.com.cuh.global.entity.TbPerson;
 import mx.com.cuh.global.service.Usuario;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 
 @org.springframework.stereotype.Controller
 
@@ -33,11 +44,13 @@ public class Controller {
 	public String index() {
 		return "index";
 	}
+	//@Value("${cosa.cosa}")
+	//private String valor;
 	
 	@GetMapping("/inicio")
 		public String inicio(Model model, @RequestParam(defaultValue = "0") int page) { //int page: sirve para especificar la página que se va a visualizar.
 																						
-		    int registrosCount = 2; //Cuantos registros queremos que aparescan por página.
+		    int registrosCount = 100; //Cuantos registros queremos que aparescan por página.
 		    Page<TbPerson> paginaPersonas = usuario.obtenerPersonasPorPagina(PageRequest.of(page, registrosCount)); //Con registrosCount cuenta la cantidad de registros que se especifico anteriormente.
 		    																										
 		    List<TbPerson> listaPersonas = paginaPersonas.getContent(); 
@@ -69,6 +82,88 @@ public class Controller {
 			return "redirect:/inicio";
 		} 
 	
+	
+	@GetMapping("/exportar-pdf")
+	public void exportarPdf(HttpServletResponse response) {
+	    try {
+	        byte[] pdfContent = generarPdf();
+	        response.setContentType("application/zip");
+	        response.setHeader("Content-Disposition", "attachment; filename = registros.zip");
+	        ZipOutputStream zipOut = new ZipOutputStream(response.getOutputStream());
+	        ZipEntry pdfEntry = new ZipEntry("registros.pdf");
+	        zipOut.putNextEntry(pdfEntry);
+	        zipOut.write(pdfContent);
+	        zipOut.closeEntry();
+	        zipOut.finish();
+	        zipOut.close();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+
+    private byte[] generarPdf() {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            Document document = new Document();
+            PdfWriter.getInstance(document, baos);
+            document.open();
+            Respuesta<TbPerson> respuesta = usuario.obtenerPersonas();
+            PdfPTable table = new PdfPTable(3);
+            addTableHeader(table);
+            addRows(table, respuesta.getListasPersona());
+            document.add(table);
+            document.close();
+
+            return baos.toByteArray();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+
+        return new byte[0];
+    }
+
+    private void addTableHeader(PdfPTable table) {
+        PdfPCell cell = new PdfPCell();
+        cell.setPhrase(new Paragraph("Nombre"));
+        table.addCell(cell);
+
+        cell = new PdfPCell();
+        cell.setPhrase(new Paragraph("Edad"));
+        table.addCell(cell);
+
+        cell = new PdfPCell();
+        cell.setPhrase(new Paragraph("Sexo"));
+        table.addCell(cell);
+    }
+
+    private void addRows(PdfPTable table, List<TbPerson> personas) {
+        int registrosMaximos = 10; //LIMITA CANTIDAD DE ELEMENTOS AL REGISTRAR
+        int registrosAgregados = 0;
+
+        for (TbPerson persona : personas) {
+            if (registrosAgregados >= registrosMaximos) {
+                break;
+            }
+
+            table.addCell(persona.getNombre());
+            table.addCell(String.valueOf(persona.getEdad()));
+            table.addCell(persona.getSexo());
+
+            registrosAgregados++;
+        }
+    }
+    /* ALMACENAR ARCHIVOS PDF EN CARPETAS TEMPORALES DENTRO DEL DISCO DURO C:
+    private void guardarPdfEnServidor(byte[] pdfContent) {
+        try {
+            Path tempDirectory = Paths.get("C:\\pdf_files");
+            Path pdfPath = tempDirectory.resolve("registros.pdf");
+            Files.write(pdfPath, pdfContent);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }*/
+
 	
 }
 			  
